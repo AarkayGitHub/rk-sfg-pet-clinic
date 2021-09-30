bcc556ccbc17c77f9f7579b4b3dbd6a19be9b1b9
Venkata Ramanuja Rao Kondapally <vkondapa@cisco.com>
2021-09-30 22:20:07 +0530

    Test Commit 2

---
+++ src/main/java/guru/springframework/rksfgpetclinic/RkSfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.rksfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkSfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkSfgPetClinicApplication.class, args);
+	}
+
+}
